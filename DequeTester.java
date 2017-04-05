4b6d4e155d5d98d8f85ae6ac9e06b6e22f21843f
TakingTheL <yliu6@stuy.edu>
2017-04-05 08:56:53 -0400

    upload of DequeTester

--- DequeTester.java
+++ DequeTester.java
@@ -24,7 +24,7 @@ public class DequeTester{
 
 	
 	/* ~~~~~~~~~ Robustness Test of toString
-	   Should print 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+	   Should print 012345678901234567890123456789
 	   Checks if your Deque can have Deques
 	   ~~~~~~~~~ */
 	Deque OfWhat = new DLLDeque();
