bcb8b19bc7bddda5b11e6f9efe53774f3b13db11
TakingTheL <yliu6@stuy.edu>
2017-04-05 08:51:57 -0400

    upload of DequeTester

---
+++ DequeTester.java
@@ -0,0 +1,45 @@
+/*~~~~~~~~~~~
+  Yuanchu Liu (Leo)
+  APCS2 pd 3
+  hw29 -- Stress is the Best
+  2017-04-04
+  ~~~~~~~~~~~ */
+
+public class DequeTester{
+    public static void main(String[] args){
+
+	/* ~~~~~~~~~ Basic operations */
+	DLLDeque ada = new DLLDeque();
+	ada.addLast("Bob");
+	ada.addLast("Cal");
+	ada.addLast("Tim");
+	ada.addLast("Zoe");
+	System.out.println(ada.pollFirst());//Bob
+	System.out.println(ada.pollFirst());//Cal
+	System.out.println(ada.peekFirst());//Tim
+	System.out.println(ada.pollFirst());//Tim
+
+
+	
+	/* ~~~~~~~~~ Robustness Test of toString
+	   Should print 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+	   Checks if your Deque can have Deques
+	   ~~~~~~~~~ */
+	Deque OfWhat = new DLLDeque();
+	Deque FromWhat = new DLLDeque();
+	Deque BelongToWhat = new DLLDeque();
+	
+	for(int i = 0; i < 10; i ++){
+	    OfWhat.addLast(i);
+	    FromWhat.addLast(i);
+	    BelongToWhat.addLast(i);
+	}
+
+	Deque<Deque> HeheXD = new DLLDeque();
+	HeheXD.addLast(OfWhat);
+	HeheXD.addLast(FromWhat);
+	HeheXD.addLast(BelongToWhat);
+	System.out.println(HeheXD);
+
+    }
+}
